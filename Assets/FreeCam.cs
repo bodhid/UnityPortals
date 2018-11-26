6ac404872b6f31d3b06ecc69da3b95a3af5e7630
Bodhi Donselaar <bodhid@hotmail.com>
2018-11-26 22:41:27 +0100

    Initial Commit

---
+++ Assets/FreeCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCam : MonoBehaviour {
+
+	private Vector3 euler=Vector3.zero;
+	void Update ()
+	{
+		euler.y += Input.GetAxis("Mouse X");
+		euler.x -= Input.GetAxis("Mouse Y");
+		euler.x = Mathf.Clamp(euler.x, -89, 89);
+		transform.localEulerAngles = euler;
+		Vector2 move = Vector2.zero;
+		float speed = Input.GetKey(KeyCode.LeftShift) ? 6f : 2f;
+		move.y -= Input.GetKey(KeyCode.S) ? 1 : 0;
+		move.y += Input.GetKey(KeyCode.W) ? 1 : 0;
+		move.x -= Input.GetKey(KeyCode.A) ? 1 : 0; 
+		move.x += Input.GetKey(KeyCode.D) ? 1 : 0;
+		transform.position += transform.forward * move.y*Time.deltaTime* speed;
+		transform.position += transform.right * move.x*Time.deltaTime* speed;
+	}
+}
---
+++ Assets/Portal.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Portal : MonoBehaviour
+{
+	public Portal connection;
+	private Camera[] cam;
+	private Transform[] camParent;
+	private RenderTexture[] buffer;
+	private Material myMaterial;
+	[Range(1,16)]
+	public int portalRenderDepth=8;
+	private int renderDepth = 8;
+	private int portalDepth=0;
+	void Start ()
+	{
+		InitializeBuffers();
+	}
+
+	private void InitializeBuffers()
+	{
+		System.GC.Collect();
+		Resources.UnloadUnusedAssets();
+		camParent = new Transform[portalRenderDepth];
+		cam = new Camera[portalRenderDepth];
+		for (int i = 0; i < cam.Length; ++i)
+		{
+			camParent[i] = new GameObject("CamParent").transform;
+			camParent[i].SetParent(transform);
+			camParent[i].localPosition = Vector3.zero;
+			GameObject portalCam = new GameObject("PortalCam");
+			cam[i] = portalCam.AddComponent<Camera>();
+			portalCam.transform.SetParent(camParent[i]);
+			portalCam.SetActive(false);
+		}
+		myMaterial = GetComponent<MeshRenderer>().material;
+		buffer = new RenderTexture[portalRenderDepth];
+		renderDepth = portalRenderDepth;
+	}
+
+	private void Update()
+	{
+		if (renderDepth != portalRenderDepth) InitializeBuffers();
+		portalDepth = -1;
+		myMaterial.mainTexture = buffer[0];
+	}
+	private void OnWillRenderObject ()
+	{
+		if (portalDepth<portalRenderDepth-1)
+		{
+			portalDepth++;
+			RenderTexture.ReleaseTemporary(buffer[portalDepth]);
+			buffer[portalDepth] = RenderTexture.GetTemporary(Screen.width, Screen.height, 32, RenderTextureFormat.ARGB32);
+			cam[portalDepth].CopyFrom(Camera.current);
+
+			int excludeLayer = (gameObject.layer == 11) ? 12 : 11;
+			cam[portalDepth].cullingMask = ~(1 << excludeLayer);
+			camParent[portalDepth].position = transform.position;
+			camParent[portalDepth].rotation = transform.rotation;
+			cam[portalDepth].transform.position = Camera.current.transform.position;
+			cam[portalDepth].transform.rotation = Camera.current.transform.rotation;
+			camParent[portalDepth].position = connection.transform.position;
+			camParent[portalDepth].eulerAngles = connection.transform.eulerAngles + new Vector3(0, 180, 0);
+			cam[portalDepth].targetTexture = buffer[portalDepth];
+
+			Texture temp = myMaterial.mainTexture;
+			myMaterial.mainTexture = buffer[(portalDepth+1)%portalRenderDepth];
+			cam[portalDepth].Render();
+			myMaterial.mainTexture = temp;
+		}
+	}
+}
